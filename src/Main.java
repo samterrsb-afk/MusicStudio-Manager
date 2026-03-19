b79eaad1ac946627d144e04a826b0d4f15e5a385
Samuel Bobbio <samuel@example.com>
2026-03-19 16:15:02 +0100

    Actualizar Hello World y preparar Dockerfile y .dockerignore

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
-public class Main
-{ public static void main (String [] args) 
-{ System.out.println("Hello MusicStudio-Manager!");
-}
-}
+public class Main {
+    public static void main(String[] args) {
+        // Creamos una 'caja' de tipo texto (String) con un nombre
+        String nombreProyecto = "MusicStudio-Manager v1.0";
+        int ramDisponible = 64; // ¡Tu potencia real!
 
+        System.out.println("--- INICIANDO SISTEMA ---");
+        System.out.println("Proyecto: " + nombreProyecto);
+        System.out.println("Memoria asignada: " + ramDisponible + "GB");
+        System.out.println("-------------------------");
+    }
+}
